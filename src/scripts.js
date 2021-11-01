b6c6ec54c0bd8ac6526dd8ce24e5cacbedb02c6e
Raul <raulfernan000@gmail.com>
2021-11-02 00:05:32 +0100

    styles.css created, index.html , scripts.js modified

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,33 @@
 const list = document.getElementById('list')
-let addButton = document.getElementById('add')
+const addButton = document.getElementById('add')
 
 addButton.onclick = function () {
     const listElement = document.getElementById('listElement').value
     let newItem = `
-    <p id='${listElement}'>${listElement}</p>
+    <div id='${listElement}'>
+        <p>${listElement}</p>
+        <button onclick="deleteItem(this)">Remove</button>
+        <button onclick="updateItem(this)">Update</button>
+        <input id="${listElement}Input">
     `
     list.innerHTML += newItem
+}
+
+function deleteItem(ele) {
+    var parEle = ele.parentNode.id
+    var delIt = document.getElementById(parEle)
+    delIt.remove()
+}
+
+function updateItem (ele) {
+    var parEleId = ele.parentNode.id
+    var parEle = document.getElementById(parEleId)
+    var upNm = document.getElementById(parEleId+"Input").value
+    parEle.innerHTML = `
+        <p>${upNm}</p>
+        <button onclick="deleteItem(this)">Remove</button>
+        <button onclick="updateItem(this)">Update</button>
+        <input id="${upNm}Input">
+    `
+    parEle.id = upNm
 }
