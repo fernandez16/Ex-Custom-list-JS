49b27085b4d125a2a1c44aa93f21d89d898892d5
Raul <raulfernan000@gmail.com>
2021-11-02 08:34:33 +0100

    script.js modified

--- src/scripts.js
+++ src/scripts.js
@@ -1,32 +1,30 @@
-const list = document.getElementById('list')
 const addButton = document.getElementById('add')
 
 addButton.onclick = function () {
     const listElement = document.getElementById('listElement').value
-    let newItem = `
+    var newItem = `
     <div id='${listElement}'>
         <p>${listElement}</p>
-        <button onclick="deleteItem(this)">Remove</button>
-        <button onclick="updateItem(this)">Update</button>
+        <button onclick="deleteItem(child)">Remove</button>
+        <button onclick="updateItem(child)">Update</button>
         <input id="${listElement}Input">
     `
     list.innerHTML += newItem
 }
 
-function deleteItem(ele) {
-    var parEle = ele.parentNode.id
-    var delIt = document.getElementById(parEle)
-    delIt.remove()
+function deleteItem(child) {
+    var parEle = document.getElementById(child.parentNode.id)
+    parEle.remove()
 }
 
-function updateItem (ele) {
-    var parEleId = ele.parentNode.id
+function updateItem (child) {
+    var parEleId = child.parentNode.id
     var parEle = document.getElementById(parEleId)
     var upNm = document.getElementById(parEleId+"Input").value
     parEle.innerHTML = `
         <p>${upNm}</p>
-        <button onclick="deleteItem(this)">Remove</button>
-        <button onclick="updateItem(this)">Update</button>
+        <button onclick="deleteItem(child)">Remove</button>
+        <button onclick="updateItem(child)">Update</button>
         <input id="${upNm}Input">
     `
     parEle.id = upNm
